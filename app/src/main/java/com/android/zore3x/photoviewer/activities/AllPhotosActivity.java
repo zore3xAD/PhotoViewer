9c40d54606770ec332af96220953505267365c8f
Anton Dobrohorskyi <zore3x@gmail.com>
2018-05-16 15:23:34 +0300

    Add allPhotosActivity for testing

---
+++ app/src/main/java/com/android/zore3x/photoviewer/activities/AllPhotosActivity.java
@@ -0,0 +1,54 @@
+package com.android.zore3x.photoviewer.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.zore3x.photoviewer.App;
+import com.android.zore3x.photoviewer.R;
+import com.android.zore3x.photoviewer.adapters.PhotosLineAdapter;
+import com.android.zore3x.photoviewer.api.Unsplash;
+import com.android.zore3x.photoviewer.api.model.Photo;
+
+import java.util.List;
+
+public class AllPhotosActivity extends AppCompatActivity{
+
+    private RecyclerView mPhotoLineRecyclerView;
+    private PhotosLineAdapter mPhotosLineAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_photos);
+
+        mPhotoLineRecyclerView = findViewById(R.id.photosLine_recyclerVeiw);
+        mPhotoLineRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        App.getUnsplash().getPhotos(1, null, null, new Unsplash.OnPhotosLoadedListener() {
+            @Override
+            public void onComplete(List<Photo> photos) {
+                updateUi(photos);
+            }
+
+            @Override
+            public void onError(String error) {
+                int d = 3;
+            }
+        });
+    }
+
+    private void updateUi(List<Photo> photos) {
+
+        if(mPhotosLineAdapter != null) {
+            mPhotosLineAdapter.setPhotoList(photos);
+            mPhotosLineAdapter.notifyDataSetChanged();
+        } else {
+            mPhotosLineAdapter = new PhotosLineAdapter(photos);
+            mPhotoLineRecyclerView.setAdapter(mPhotosLineAdapter);
+        }
+
+    }
+}
