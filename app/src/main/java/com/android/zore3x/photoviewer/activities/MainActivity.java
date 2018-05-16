365ae0dc7ea703a3f82acbfffc7a6bf3ed92f04f
Anton Dobrohorskyi <zore3x@gmail.com>
2018-05-16 15:23:55 +0300

    Modify Main Activity for testing

--- app/src/main/java/com/android/zore3x/photoviewer/activities/MainActivity.java
+++ app/src/main/java/com/android/zore3x/photoviewer/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.android.zore3x.photoviewer.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.zore3x.photoviewer.App;
 import com.android.zore3x.photoviewer.R;
@@ -16,42 +19,22 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private RecyclerView mPhotoLineRecyclerView;
-    private PhotosLineAdapter mPhotosLineAdapter;
+    private Button mAllPhotosButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPhotoLineRecyclerView = findViewById(R.id.photosLine_recyclerVeiw);
-        mPhotoLineRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        mAllPhotosButton = findViewById(R.id.allPhoto_button);
 
-
-
-        App.getUnsplash().getPhotos(1, null, null, new Unsplash.OnPhotosLoadedListener() {
+        mAllPhotosButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(List<Photo> photos) {
-                updateUi(photos);
-            }
-
-            @Override
-            public void onError(String error) {
-                int d = 3;
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AllPhotosActivity.class);
+                startActivity(intent);
             }
         });
-    }
-
-    private void updateUi(List<Photo> photos) {
-
-        if(mPhotosLineAdapter != null) {
-            mPhotosLineAdapter.setPhotoList(photos);
-            mPhotosLineAdapter.notifyDataSetChanged();
-        } else {
-            mPhotosLineAdapter = new PhotosLineAdapter(photos);
-            mPhotoLineRecyclerView.setAdapter(mPhotosLineAdapter);
-        }
 
     }
 }
