7bbb6d36549731d5eaa12b80586c6c111ccfdd46
Anton Dobrohorskyi <zore3x@gmail.com>
2018-05-16 14:57:58 +0300

    Functional testing

--- app/src/main/java/com/android/zore3x/photoviewer/activities/MainActivity.java
+++ app/src/main/java/com/android/zore3x/photoviewer/activities/MainActivity.java
@@ -2,8 +2,11 @@ package com.android.zore3x.photoviewer.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.android.zore3x.photoviewer.R;
+import com.android.zore3x.photoviewer.adapters.PhotosLineAdapter;
 import com.android.zore3x.photoviewer.api.Order;
 import com.android.zore3x.photoviewer.api.Unsplash;
 import com.android.zore3x.photoviewer.api.model.Photo;
@@ -14,16 +17,22 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String CLIENT_ID = "e83deeb4b720fde9e40ae1cb8ea68b9d5cb8357b5415cdfd567881b234c9bbbc";
 
+    private RecyclerView mPhotoLineRecyclerView;
+    private PhotosLineAdapter mPhotosLineAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mPhotoLineRecyclerView = findViewById(R.id.photosLine_recyclerVeiw);
+        mPhotoLineRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
         Unsplash unsplash = new Unsplash(CLIENT_ID);
         unsplash.getPhotos(1, null, null, new Unsplash.OnPhotosLoadedListener() {
             @Override
             public void onComplete(List<Photo> photos) {
-                int d =4;
+                updateUi(photos);
             }
 
             @Override
@@ -31,5 +40,17 @@ public class MainActivity extends AppCompatActivity {
                 int d = 3;
             }
         });
+    }
+
+    private void updateUi(List<Photo> photos) {
+
+        if(mPhotosLineAdapter != null) {
+            mPhotosLineAdapter.setPhotoList(photos);
+            mPhotosLineAdapter.notifyDataSetChanged();
+        } else {
+            mPhotosLineAdapter = new PhotosLineAdapter(photos);
+            mPhotoLineRecyclerView.setAdapter(mPhotosLineAdapter);
+        }
+
     }
 }
